32d00bc06d892e9a391b8083c1345a68db6f4a61
PENGUII\alpte <alptekin.eroll@gmail.com>
2025-07-17 17:38:13 +0300

    Development-1003 / Infrastructure layer created.

---
+++ Project.Infrastructure/ElasticClientFactory.cs
@@ -0,0 +1,15 @@
+﻿using Elastic.Clients.Elasticsearch;
+using Microsoft.Extensions.Configuration;
+
+namespace Project.Infrastructure;
+public static class ElasticClientFactory
+{
+    public static ElasticsearchClient CreateClient(IConfiguration config)
+    {
+        var uri = config["Elastic:Uri"];
+        var settings = new ElasticsearchClientSettings(new Uri(uri))
+            .DefaultIndex("products");
+
+        return new ElasticsearchClient(settings);
+    }
+}
